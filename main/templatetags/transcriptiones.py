92cd0b1df7c6615e673bbca841650c6fa0207e69
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-04-27 09:40:00 +0200

    Removed unused template tags

--- main/templatetags/transcriptiones.py
+++ main/templatetags/transcriptiones.py
@@ -47,15 +47,3 @@ class CardNode(template.Node):
             '  </div>' \
             '</div>'
         return output
-
-
-@register.inclusion_tag('text_card.html')
-def text_card(title, text):
-    """Provides a bootstrap Card to display static text."""
-    return {'title': title, 'text': text}
-
-
-@register.simple_tag
-def get_verbose_name(transcriptiones_object, field_name):
-    """Provides the possibility to get the (translated) verbose name of a model object. """
-    return transcriptiones_object._meta.get_field(field_name).verbose_name
