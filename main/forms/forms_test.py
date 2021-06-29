60203ad31523e1b43e54897f60a307fea69aa9bb
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-06-29 11:16:10 +0200

    Moved and renamed test upload form

--- main/forms/forms_test.py
+++ main/forms/forms_test.py
@@ -7,35 +7,7 @@ from main.models import Institution, \
     RefNumber, Document, SourceType, Author, Language
 
 
-class InstSelectTestForm(forms.ModelForm):
-    parent_institution = forms.ModelChoiceField(queryset=Institution.objects.all().order_by('institution_name'),
-                                                widget=autocomplete.ModelSelect2(url='main:inst-autocomplete'))
 
-    parent_ref_number = forms.ModelChoiceField(queryset=RefNumber.objects.all().order_by('ref_number_name'),
-                                               widget=autocomplete.ModelSelect2(url='main:refn-autocomplete',
-                                                                                forward=['parent_institution', ]))
-
-    selection_helper_source_type = forms.ModelChoiceField(queryset=SourceType.objects.filter(parent_type=None).order_by('type_name'),
-                                                          widget=autocomplete.ModelSelect2(url='main:srctype-autocomplete'))
-
-    source_type = forms.ModelChoiceField(queryset=SourceType.objects.exclude(parent_type=None).order_by('type_name'),
-                                         widget=autocomplete.ModelSelect2(url='main:srctype-ch-autocomplete',
-                                                                          forward=['selection_helper_source_type', ]))
-
-    author = forms.ModelMultipleChoiceField(queryset=Author.objects.all().order_by('author_name'),
-                                            widget=autocomplete.ModelSelect2Multiple(url='main:author-autocomplete'))
-
-    language = forms.ModelMultipleChoiceField(queryset=Language.objects.all().order_by('name_native'),
-                                              widget=autocomplete.ModelSelect2Multiple(url='main:language-autocomplete'))
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = initialize_form_helper()
-
-    class Meta:
-        model = Document
-        exclude = ('institution_utc_add', 'document_slug', 'version_number', 'commit_message')
 
 
 class InstModelForm(BSModalModelForm):
