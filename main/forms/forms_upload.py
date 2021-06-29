f4a92ffe12767a6bbcda369aa22ccd15a4cfef5e
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-06-29 11:16:28 +0200

    Added remodeled upload form

--- main/forms/forms_upload.py
+++ main/forms/forms_upload.py
@@ -2,12 +2,46 @@ from bootstrap_modal_forms.mixins import CreateUpdateAjaxMixin
 from ckeditor.widgets import CKEditorWidget
 from django import forms
 from django.utils.translation import ugettext as _
-from main.models import Institution, RefNumber, Document, SourceType
-from main.widgets import SourceChildSelect
-from main.forms.forms_helper import initialize_form_helper, get_popover_html
 from bootstrap_modal_forms.forms import BSModalModelForm
 from dal import autocomplete
 from django.utils.text import slugify
+
+from main.models import Institution, RefNumber, Document, SourceType, Author, Language
+from main.widgets import SourceChildSelect
+from main.forms.forms_helper import initialize_form_helper, get_popover_html
+
+
+class UploadTranscriptionForm(forms.ModelForm):
+    """This is the upload form to upload a new transcribed document."""
+    parent_institution = forms.ModelChoiceField(queryset=Institution.objects.all().order_by('institution_name'),
+                                                widget=autocomplete.ModelSelect2(url='main:inst-autocomplete'))
+
+    parent_ref_number = forms.ModelChoiceField(queryset=RefNumber.objects.all().order_by('ref_number_name'),
+                                               widget=autocomplete.ModelSelect2(url='main:refn-autocomplete',
+                                                                                forward=['parent_institution', ]))
+
+    selection_helper_source_type = forms.ModelChoiceField(queryset=SourceType.objects.filter(parent_type=None).order_by('type_name'),
+                                                          widget=autocomplete.ModelSelect2(url='main:srctype-autocomplete'))
+
+    source_type = forms.ModelChoiceField(queryset=SourceType.objects.exclude(parent_type=None).order_by('type_name'),
+                                         widget=autocomplete.ModelSelect2(url='main:srctype-ch-autocomplete',
+                                                                          forward=['selection_helper_source_type', ]))
+
+    author = forms.ModelMultipleChoiceField(queryset=Author.objects.all().order_by('author_name'),
+                                            widget=autocomplete.ModelSelect2Multiple(url='main:author-autocomplete'))
+
+    language = forms.ModelMultipleChoiceField(queryset=Language.objects.all().order_by('name_native'),
+                                              widget=autocomplete.ModelSelect2Multiple(url='main:language-autocomplete'))
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = initialize_form_helper()
+
+    class Meta:
+        model = Document
+        exclude = ('institution_utc_add', 'document_slug', 'version_number', 'commit_message')
+
 
 class InstitutionForm2(BSModalModelForm):
     class Meta:
