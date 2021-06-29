afef4ae87e455fee79faa2e7bf3e25949a60d067
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-06-29 21:26:04 +0200

    Added ref number filter

--- main/filters.py
+++ main/filters.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django_filters import FilterSet, CharFilter, DateRangeFilter, DateFilter
-from main.models import Institution, Document
+from main.models import Institution, RefNumber, Document
 
 
 class InstitutionFilter(FilterSet):
@@ -9,6 +10,18 @@ class InstitutionFilter(FilterSet):
     class Meta:
         model = Institution
         fields = ['institution_name']
+
+
+class RefNumberFilter(FilterSet):
+    """Filter to filter a reference number table"""
+    ref_number_name = CharFilter(method='multi_filter')
+
+    class Meta:
+        model = RefNumber
+        fields = ['ref_number_name']
+
+    def multi_filter(self, queryset, name, value):
+        return queryset.filter(Q(ref_number_name__icontains=value) | Q(ref_number_title__icontains=value))
 
 
 class DocumentFilter(FilterSet):
