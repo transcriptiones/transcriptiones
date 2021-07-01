4b59b7ae5d39c0774d3b9479b3b6405d79762170
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-07-01 08:54:40 +0200

    worked on subscriptions

--- main/views/views_subscriptions.py
+++ main/views/views_subscriptions.py
@@ -1,15 +1,38 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.utils.translation import ugettext as _
+
+from main.models import Document, RefNumber, User, UserSubscription
 
 
 @login_required
-def subscribe_ref_number_view(request, pk):
-
+def subscriptions(request):
     return HttpResponse("niy")
 
 
 @login_required
+def subscribe_ref_number_view(request, pk):
+    try:
+        ref_number = RefNumber.objects.get(pk=pk)
+        UserSubscription.objects.create(user=request.user,
+                                        subscription_type=UserSubscription.SubscriptionType.REF_NUMBER,
+                                        object_id=ref_number.id)
+        messages.success(request, _('Subscription added'))
+    except RefNumber.DoesNotExist:
+        messages.error(request, _('Could not add subscription'))
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
 def subscribe_document_view(request, pk):
+    try:
+        document = Document.objects.get(pk=pk)
+    except RefNumber.DoesNotExist:
+        document = None
+
     return HttpResponse("niy")
 
 
