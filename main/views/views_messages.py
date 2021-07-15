50db29fad8acdec6847d94d9f17b8b6428500220
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2021-07-15 22:18:58 +0200

    Implemented send message functionality

--- main/views/views_messages.py
+++ main/views/views_messages.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.utils.translation import ugettext as _
 
-from main.forms.forms_user import UserMessageOptionsForm
+from main.forms.forms_user import UserMessageOptionsForm, WriteMessageForm
 from main.tables import UserMessageTable
-from main.models import Document, RefNumber, User, UserSubscription, UserMessage
+from main.models import User, UserMessage
 
 
 @login_required
-def messages(request):
-    table = UserMessageTable(data=UserMessage.objects.filter(user=request.user))
+def messages_view(request):
+    table = UserMessageTable(data=UserMessage.objects.filter(receiving_user=request.user))
     form = UserMessageOptionsForm()
 
     if request.method == 'POST':
@@ -21,6 +21,35 @@ def messages(request):
 
 
 @login_required
+def notifications_view(request):
+    table = UserMessageTable(data=UserMessage.objects.filter(receiving_user=request.user))
+    form = UserMessageOptionsForm()
+
+    if request.method == 'POST':
+        form = UserMessageOptionsForm(request.POST)
+
+    return render(request, 'main/users/notifications.html', {'table': table, 'form': form})
+
+
+@login_required
 def delete_all_view(request):
     return HttpResponse('NIY')
+
+
+@login_required
+def message_user_view(request, username):
+    receiving_user = User.objects.get(username=username)
+    form = WriteMessageForm(user=receiving_user)
+
+    if request.method == "POST":
+        form = WriteMessageForm(request.POST, user=receiving_user)
+        if form.is_valid():
+            new_message = UserMessage.objects.create(sending_user=request.user,
+                                                     receiving_user=receiving_user,
+                                                     subject=form.cleaned_data['subject'],
+                                                     message=form.cleaned_data['message'])
+            messages.success(request, _('Your message has been sent.'))
+            return redirect(request.META.get('HTTP_REFERER'))
+
+    return render(request, 'main/users/write_message.html', {'form': form})
 
